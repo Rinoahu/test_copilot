a968914b2ee6b1974fc7f8b8b8d59e438af3b026
Xiao <xiaohu@iastate.edu>
2022-09-29 23:03:29 -0500

    debug

---
+++ simd_test/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ $1 ]
+then
+    flag=$1
+else
+    flag=1
+fi
+
+if [ $2 ]
+then
+    cc=$2
+else
+    cc='gcc'
+fi
+
+echo $flag $cc
+ln -sf simd_list.cpp simd.cpp
+rm -f *.exe
+
+
+if [ $cc == "gcc" ]
+then
+    g++ -O$flag -mavx2 -fopt-info-vec-optimized -ftree-vectorizer-verbose=2 -o a_serial.exe simd.cpp &> log.txt
+    g++ -O$flag -mavx2 -fopenmp -fopt-info-vec-optimized -ftree-vectorizer-verbose=2 -o a_simd.exe simd.cpp &>> log.txt
+
+elif [ $cc == "clang" ]
+then
+    clang++-16 -O$flag -mavx2 -Rpass="loop|vect" -Rpass-missed="loop|vect" -Rpass-analysis="loop|vect" -o a_serial.exe simd.cpp &> log.txt
+    #echo serial compiled
+    clang++-16 -O$flag -mavx2 -Rpass="loop|vect" -Rpass-missed="loop|vect" -Rpass-analysis="loop|vect" -fopenmp -o a_simd.exe simd.cpp &>> log.txt
+    #echo simd compiled
+else
+    icpc -O$flag -mavx2 -D NOFUNCCALL -qopt-report=1 -qopt-report-phase=vec -o a_serial.exe simd.cpp && mv simd.optrpt serial.optrpt
+    icpc -O$flag -mavx2 -D NOFUNCCALL -qopt-report=1 -qopt-report-phase=vec -qopenmp -o a_simd.exe simd.cpp
+fi
+
+
+echo serial
+./a_serial.exe
+echo simd
+./a_simd.exe
+
+
+rm simd.cpp *.exe
