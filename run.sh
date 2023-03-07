db121cbd179491e5cafa52406e26059f5f9a2913
Xiao <xiaohu@iastate.edu>
2023-03-07 14:34:31 -0600

    debug

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-gcc -O1 -fopenmp -o test_omp test_omp_simd.c && ./test_omp
+if [ $1 ]
+then
+    o=$1
+else
+    o=1
+fi
+
+cc=/usr/bin/clang
+
+rm -rf test_omp
+$cc -O$o -fopenmp -o test_omp test_omp_simd.c
+./test_omp
+
+rm test_omp
--- test_omp_simd.c
+++ test_omp_simd.c
@@ -4,19 +4,22 @@
 #include <stdlib.h>
 
 int main() {
-  int N = 1000000000; // size of the array
+  int N = 500000000; // size of the array
   int *A = malloc(N * sizeof(int)); // the array to check
   int nonzero; // flag to indicate if all elements are nonzero
 
-  clock_t start, end;
-  double cpu_time_used;
+  clock_t s0, e0, s1, e1;
+  double t0, t1;
 
   // initialize the array with some values
   for (int i = 0; i < N; i++) {
     A[i] = i + 1;
   }
 
-  start = clock(); // get the start time
+  printf("%ld %d\n", CLOCKS_PER_SEC, N);
+
+
+  s0 = clock(); // get the start time
 
   for (int j = 0; j < 10; j++) {
 
@@ -29,9 +32,28 @@ int main() {
 
   }
 
-  end = clock(); // get the end time
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; // calculate the elapsed time in seconds
-  printf("The work took %f seconds to execute.\n", cpu_time_used);
+  e0 = clock(); // get the end time
+  t0 = ((double) (e0 - s0)) / CLOCKS_PER_SEC; // calculate the elapsed time in seconds
+  printf("%ld %ld The simd took %f seconds to execute.\n", s0, e0, t0);
+
+
+  s1 = clock(); // get the start time
+
+  for (int j = 0; j < 10; j++) {
+
+      // break;
+      nonzero = 1;
+      for (int i = 0; i < N; i++) {
+        nonzero &= (A[i] != 0); // bitwise and with the flag
+      }
+
+  }
+
+  e1 = clock(); // get the end time
+  t1 = ((double) (e1 - s1)) / CLOCKS_PER_SEC; // calculate the elapsed time in seconds
+  printf("%ld %ld  No simd took %f seconds to execute.\n", s1, e1, t1);
+
+
 
   // print the result
   if (nonzero) {
